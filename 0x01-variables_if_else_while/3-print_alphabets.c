0e3c8ebb5f1515ecdddc40c7daf4d8d252ad02e0
dammysaina <dammyadediran94@gmail.com>
2022-04-08 16:41:05 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+/* betty style doc for function main goes there */
 /**
  *  *  *  *  * main - Entry point
- *   *   *   *   *   *
- *    *    *    *    *    * Return: Always 0 (Success) 
+ *   *   *   *   *   
+ *    *    *    *    * Return: Always 0 (Success) 
 */
 int main(void)
 {
