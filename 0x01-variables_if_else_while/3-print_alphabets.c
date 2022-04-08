07d867d69b77e29c961eabbb9e489df62c8d0fb3
dammysaina <dammyadediran94@gmail.com>
2022-04-08 16:13:40 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-	char upper;
-	char lower;
+char upper;
+char lower;
 
-	for (lower = 'a'; lower <= 'z'; lower++)
-	{
-			putchar(lower);
-	}
-	for (upper = 'A'; upper <= 'Z'; upper++)
-	{
-			putchar(upper);
-	}
-	putchar('\n');
-	return (0);
+for (lower = 'a'; lower <= 'z'; lower++)
+{
+	putchar(lower);
+}
+for (upper = 'A'; upper <= 'Z'; upper++)
+{
+	putchar(upper);
+}
+putchar('\n');
+return (0);
 }
