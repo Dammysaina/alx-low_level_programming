81eac71beb907c056dfaf8a784407c9fea6eaf42
dammysaina <dammyadediran94@gmail.com>
2022-04-08 17:12:43 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,15 @@ int main(void)
 {
 int x;
 
-for (x = 0; x <= 10; x++)
+for (x = 0; x < 10; x++)
 {
-	putchar(x);
-	if (x =! '9')
+	putchar((x % 10) + '0');
+	if (x < 9)
 	{
 		putchar(0x2C);
 		putchar(0x20);
 	}
 }
+putchar('\n');
 return (0);
 }
