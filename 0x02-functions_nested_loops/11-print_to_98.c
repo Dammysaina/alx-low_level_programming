f4eded510178341d131093bd4a1d27b3c74799b3
dammysaina <dammyadediran94@gmail.com>
2022-04-12 18:12:29 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  *  * print_to_98 - Check Holberton
@@ -10,27 +9,16 @@
  */
 void print_to_98(int n)
 {
-	if (n <= 98)
+	if (n >= 98)
 	{
-		for (; n <= 98; n++)
-		{
-			printf("%d", n);
-			if (n == 98)
-				continue;
-			printf(", ");
-			printf("\n");
-		}
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
 	}
-		else
-		{
-			for (; n >= 98; n--)
-			{
-				printf("%d", n);
-				
-				if (n == 98)
-					continue;
-				printf(", ");
-			}
-			printf("\n");
-		}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
 }
