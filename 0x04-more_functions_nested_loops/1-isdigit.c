e687e9c5ec4fc537d0c4e3a3a5b7f442cfa25cfd
dammysaina <dammyadediran94@gmail.com>
2022-04-14 13:14:31 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,19 +3,17 @@
 /**
  *  * _isdigit - A function that checks for a digit (0 through 9).
  *   * @c: An input integer
- *    * Return: 1 if c is a digit or 0 otherwise
-  */
-int _isdigit(int c)
+ *    * Return: 1 if c is a digit or 0 otherwise 
+ */
+int _isdigit(int n)
 {
-	char i = '0';
-	int isdigit = 0;
-	for (; i <= '9'; i++)
+	int i;
+	for (i = '0'; i <= '9'; i++)
 	{
-		if (i == c)
+		if (n == i)
 		{
-			isdigit = 1;
-			break;
+			return (1);
 		}
 	}
-	return (isdigit);
+	return (0);
 }
