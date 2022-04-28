beeb288283ca5931f50abdd48f09379a57ec325a
dammysaina <dammyadediran94@gmail.com>
2022-04-28 02:31:39 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -53,7 +53,9 @@ void iterate_wild(char **wildstr)
 char *postfix_match(char *str, char *postfix)
 {
 	int str_len = strlen_no_wilds(str) - 1;
-	int postfix_len = strlen_no_wilds(postfix) - 1;if (*postfix == '*')
+	int postfix_len = strlen_no_wilds(postfix) - 1;
+	
+	if (*postfix == '*')
 		iterate_wild(&postfix);
 	if (*(str + str_len - postfix_len) == *postfix && *postfix != '\0')
 	{
