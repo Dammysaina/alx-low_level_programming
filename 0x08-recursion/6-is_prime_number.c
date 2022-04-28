1e124bc0b4df3e07db5fffe1e057373e7d4950ae
dammysaina <dammyadediran94@gmail.com>
2022-04-28 01:17:13 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,7 +11,6 @@ int is_prime_number(int n);
 int is_prime_number(int n)
 {
 	int div = 2;
-	
 	if (n <= 1)
 		return (0);
 	if (n <= 3)
@@ -30,7 +29,6 @@ int is_divisible(int num, int div)
 	if (num % div == 0)
 		return (0);
 	if (div == num / 2)
-		return (1);
-	
+		return (1);	
 	return (is_divisible(num, div + 1));
 }
