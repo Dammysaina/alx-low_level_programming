f1088ca1d711a1b11f80532541e138cee95e37c5
dammysaina <dammyadediran94@gmail.com>
2022-04-28 00:56:00 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -15,10 +15,8 @@ int find_sqrt(int num, int root)
 {
 	if ((root * root) == num)
 		return (root);
-	
 	if (root == num / 2)
 		return (-1);
-	
 	return (find_sqrt(num, root + 1));
 }
 
@@ -32,10 +30,8 @@ int find_sqrt(int num, int root)
 int _sqrt_recursion(int n)
 {
 	int root = 0;
-	
 	if (n < 0)
 		return (-1);
-	
 	if (n == 1)
 		return (1);
 	return (find_sqrt(n, root));
