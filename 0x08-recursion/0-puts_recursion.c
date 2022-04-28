cc38133a0354c75dacb61c7a0c3fb2b8d9fe5a52
dammysaina <dammyadediran94@gmail.com>
2022-04-28 00:17:14 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *  * _puts_recursion - a function that prints a string, followed by a new line.
+ *  * _puts_recursion - Prints a string, followed by a new line.
  *   * @s: An input string to printing
  *    * Return: Nothing
  */
