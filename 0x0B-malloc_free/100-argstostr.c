b462251929ca4a99255906caba22af9452b7cd94
dammysaina <dammyadediran94@gmail.com>
2022-05-05 07:04:58 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -15,28 +15,25 @@ char *argstostr(int ac, char **av)
 		if (ac <= 0 || av == NULL)
 			return (NULL);
 		for (; i < ac; i++)
-							{
-										for (j = 0; av[i][j]; j++)
-														len++;
-												len++;
-													}
-
-						len++;
-							new_str = malloc(len * sizeof(char));
-								if (new_str == NULL)
-											return (NULL);
-
-									for (i = 0; i < ac; i++)
-											{
-														for (j = 0; av[i][j]; j++)
-																	{
-																					new_str[k] = av[i][j];
-																								k++;
-																										}
-																new_str[k] = '\n';
-																		k++;
-																			}
-
-										new_str[k] = '\0';
-											return (new_str);
+		{
+			for (j = 0; av[i][j]; j++)
+				len++;
+			len++;
+		}
+		len++;
+		new_str = malloc(len * sizeof(char));
+		if (new_str == NULL)
+			return (NULL)
+				for (i = 0; i < ac; i++)
+				{
+					for (j = 0; av[i][j]; j++)
+					{
+						new_str[k] = av[i][j];
+						k++;
+					}
+					new_str[k] = '\n';
+					k++;
+				}
+		new_str[k] = '\0';
+		return (new_str);
 }
